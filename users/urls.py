9b63356b37bab68359b91248f6236ea21a70b4c7
Maxim <maksimsycev98@gmail.com>
2026-07-14 21:10:30 +0600

    Сделал 7ое дз

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/users/register', views.registration_api_view),
-    path('api/v1/users/authorize', views.authorization_api_view),
-    path('api/v1/users/confirm', views.confirmation_api_view),
+    path('api/v1/users/register', views.RegistrationAPIView.as_view()),
+    path('api/v1/users/authorize', views.AuthorizationAPIView.as_view()),
+    path('api/v1/users/confirm', views.ConfirmationAPIView.as_view()),
 ]
